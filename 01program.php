70fdd316153666571c3cfc80d1268d4aeee02e8d
paulpaw <49188880+paulpaw@users.noreply.github.com>
2019-05-21 11:07:03 +0200

    Piąty commit

--- 01program.php
+++ 01program.php
@@ -4,5 +4,6 @@ echo ("<br> Jestem programem 01");
 echo ("<br> Test 11111");
 echo ("<br> TEST 22222");
 echo ("<br> Dalsza czesc programu");
+echo ("<br> Dalsza czesc programu 22222 z GiHuba");
 ?>
 
